2f69c69fa24eec0649cd27ab3da01a25432d3754
Nicolas <nweissbueno@gmail.com>
2025-02-19 15:16:07 -0300

    Feito gerador de senhas aleatorias modo easy e hard

--- Seções/Seção_5.py
+++ Seções/Seção_5.py
@@ -1,7 +1,7 @@
 #you have to make a password generator with random letters, numbers and symbols.
 import random
 
-senha = ""
+senha_easy = []
 
 numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
@@ -15,12 +15,16 @@ nr_symbols = int(input("Digite a quantidade de símbolos da senha desejada: "))
 
 
 for i in range(nr_letters):
-    senha += random.choice(letters)
+    senha_easy.append(random.choice(letters))
     
 for i in range(nr_numbers):
-    senha += random.choice(numbers)
+    senha_easy.append(random.choice(numbers))
     
 for i in range(nr_symbols):
-    senha += random.choice(symbols)
+    senha_easy.append(random.choice(symbols))
     
-print(senha)
+print(senha_easy)
+
+senha_hard = random.sample(senha_easy, len(senha_easy))
+
+print(senha_hard)
